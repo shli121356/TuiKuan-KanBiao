b5a59f0cbf2797ce8a444882a571f865554ffbfa
Mr.xiang.zi <70995413+shli121356@users.noreply.github.com>
2026-08-10 15:31:47 +0800

    Configure GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 import { openFilePlugin } from './vite-plugin-open-file';
 
 export default defineConfig({
+  base: '/TuiKuan-KanBiao/',
   plugins: [react(), openFilePlugin()],
 });
